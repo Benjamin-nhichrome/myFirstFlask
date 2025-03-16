07de4801107ae30922370fa4f23460c2c64fc4db
Benjamin-nhichrome <adusubenjamin8@gmail.com>
2025-03-16 18:53:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/templates/greet.html', methods=['POST'])
+def greet():
+    current_time = datetime.now().strftime('%H:%M:%S')
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    name = request.form.get('name')
+
+    return render_template('greet.html', name=name, current_time=current_time, current_date=current_date)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
+
